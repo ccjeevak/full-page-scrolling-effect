7d392e230ad7181c6e84ffb0899f4c6f8530ba87
code-camp <34296056+ccjeevak@users.noreply.github.com>
2023-11-06 12:46:07 +0530

    Add files via upload
    folders added

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className='container'>
+        <section>
+          <div className='content'>
+              <h2>Section One</h2>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore, perferendis sint quae qui consequatur nostrum similique repellat, voluptatibus molestiae necessitatibus omnis ipsum deleniti adipisci! Exercitationem expedita sunt dolorem recusandae est.</p>
+          </div>
+        </section>
+        <section>
+          <div className='content'>
+              <h2>Section Two</h2>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore, perferendis sint quae qui consequatur nostrum similique repellat, voluptatibus molestiae necessitatibus omnis ipsum deleniti adipisci! Exercitationem expedita sunt dolorem recusandae est.</p>
+          </div>
+        </section>
+        <section>
+          <div className='content'>
+              <h2>Section Three</h2>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore, perferendis sint quae qui consequatur nostrum similique repellat, voluptatibus molestiae necessitatibus omnis ipsum deleniti adipisci! Exercitationem expedita sunt dolorem recusandae est.</p>
+          </div>
+        </section>
+        <section>
+          <div className='content'>
+              <h2>Section Four</h2>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore, perferendis sint quae qui consequatur nostrum similique repellat, voluptatibus molestiae necessitatibus omnis ipsum deleniti adipisci! Exercitationem expedita sunt dolorem recusandae est.</p>
+          </div>
+        </section>
+        <section>
+          <div className='content'>
+              <h2>Section Five</h2>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore, perferendis sint quae qui consequatur nostrum similique repellat, voluptatibus molestiae necessitatibus omnis ipsum deleniti adipisci! Exercitationem expedita sunt dolorem recusandae est.</p>
+          </div>
+        </section>
+    </div>
+  )
+}
+
+export default App
